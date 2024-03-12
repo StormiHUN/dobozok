45997394d631c92541ab06f5a326f9aeb17c5f84
StormiHUN <stormihun@gmail.com>
2024-03-12 13:11:38 +0100

    Kattint

--- src/main/java/com/example/dobozok/HelloController.java
+++ src/main/java/com/example/dobozok/HelloController.java
@@ -21,6 +21,7 @@ public class HelloController {
 
     Label[][] t = new Label[10][15];
     String[][] tn = new String[10][15];
+    int[][] ticks = new int[10][15];
 
     long step = 0;
     AnimationTimer timer = new AnimationTimer() {
@@ -29,6 +30,20 @@ public class HelloController {
             if(l > step){
                 step = l + 100_000_000;
                 arrow.setRotate(arrow.getRotate()+36);
+                for(int y = 0; y < 10; y++){
+                    for(int x = 0; x < 15; x++){
+                        if(ticks[y][x] != -1){
+                            if(ticks[y][x] == 10){
+                                tn[y][x] = "icons/null.png";
+                                t[y][x].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("icons/null.png"))));
+                                open++;
+                                ticks[y][x] = -2;
+                            }
+                            ticks[y][x]++;
+                        }
+                    }
+                }
+                openCount.setText(open+" db");
             }
         }
     };
@@ -41,10 +56,12 @@ public class HelloController {
                 tn[y][x] = "icons/null.png";
                 t[y][x].setLayoutX(10+x*64);
                 t[y][x].setLayoutY(10+y*64);
+                ticks[y][x] = -1;
                 int finalY = y;
                 int finalX = x;
                 t[y][x].setOnMouseEntered(event -> t[finalY][finalX].setStyle("-fx-background-color: lightgreen"));
                 t[y][x].setOnMouseExited(event -> t[finalY][finalX].setStyle("-fx-background-color: white"));
+                t[y][x].setOnMousePressed(event -> katt(finalY,finalX));
                 pane.getChildren().add(t[y][x]);
             }
         }
@@ -52,11 +69,45 @@ public class HelloController {
         timer.start();
     }
 
+    public void katt(int y, int x){
+        if(y == 9 && tn[y][x] == "icons/null.png"){
+            tn[y][x] = "icons/box.png";
+            closed++;
+            t[y][x].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("icons/box.png"))));
+            closedCount.setText(closed+" db");
+            return;
+        }
+        if(tn[y][x] == "icons/null.png" && tn[y+1][x] == "icons/box.png"){
+            tn[y][x] = "icons/box.png";
+            t[y][x].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("icons/box.png"))));
+            closed++;
+            closedCount.setText(closed+" db");
+            return;
+        }
+        if(y == 0){
+            if(tn[y][x] == "icons/box.png"){
+                tn[y][x] = "icons/boxopen.png";
+                t[y][x].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("icons/boxopen.png"))));
+                ticks[y][x]++;
+            }
+            return;
+        }
+        if(y != 0){
+            if(tn[y-1][x] == "icons/null.png" && tn[y][x] == "icons/box.png"){
+                tn[y][x] = "icons/boxopen.png";
+                t[y][x].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("icons/boxopen.png"))));
+                ticks[y][x]++;
+            }
+            return;
+        }
+    }
+
     public void onRandomClick() {
         for(int y = 0; y < 10; y++){
             for(int x = 0; x < 15; x++){
                 t[y][x].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("icons/null.png"))));
                 tn[y][x] = "icons/null.png";
+                ticks[y][x] = -1;
             }
         }
         for(int x = 0; x < 15; x++){
